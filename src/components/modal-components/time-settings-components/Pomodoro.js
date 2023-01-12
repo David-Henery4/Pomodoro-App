0c049a9527544faf57ab12a998f0e0922ffb94ac
DizzlyD <david4henery00@hotmail.com>
2023-01-12 10:36:00 +0000

    pre-time-settings-set-up

--- src/components/modal-components/time-settings-components/Pomodoro.js
+++ src/components/modal-components/time-settings-components/Pomodoro.js
@@ -1,24 +1,30 @@
-import React from 'react'
+import React from "react";
 import { ArrowDownIcon, ArrowUpIcon } from "../../../assets";
 
-const Pomodoro = ({pomodoroMins,setPomodoroMins}) => {
+const Pomodoro = ({ pomodoroMins, setPomodoroMins }) => {
   return (
     <div className="w-full flex justify-between items-center smlTab:flex-col smlTab:gap-[10px] smlTab:w-auto smlTab:items-start">
       <p className="text-xs text-lighterBlue/40">pomodoro</p>
       <div className="bg-lightGrey rounded-[10px] py-2 px-3 w-[140px] flex items-center justify-between">
         <p>{pomodoroMins}</p>
         <div className="flex flex-col gap-[9px]">
-          <ArrowUpIcon className="hover:cursor-pointer" onClick={() => {
-            setPomodoroMins(pomodoroMins + 1)
-          }}/>
-          <ArrowDownIcon className="hover:cursor-pointer" onClick={() => {
-            setPomodoroMins(pomodoroMins - 1);
-            if (pomodoroMins <= 0)setPomodoroMins(0)
-          }}/>
+          <ArrowUpIcon
+            className="hover:cursor-pointer"
+            onClick={() => {
+              setPomodoroMins(pomodoroMins + 1);
+            }}
+          />
+          <ArrowDownIcon
+            className="hover:cursor-pointer"
+            onClick={() => {
+              setPomodoroMins(pomodoroMins - 1);
+              if (pomodoroMins <= 0) setPomodoroMins(0);
+            }}
+          />
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Pomodoro
+export default Pomodoro;
