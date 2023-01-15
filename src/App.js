20bbdaa0b0fadbd52c548a2dcda567f396eacccf
DizzlyD <david4henery00@hotmail.com>
2023-01-15 22:29:14 +0000

    added-padding

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ function App() {
     <div className={`App ${activeFont.name} font-bold text-darkGrey w-full min-h-screen bg-lighterBlue`}>
       <Overlay isSettingsModalOpen={isSettingsModalOpen} setIsSettingsModalOpen={setIsSettingsModalOpen}/>
       <SettingsModal isSettingsModalOpen={isSettingsModalOpen} setIsSettingsModalOpen={setIsSettingsModalOpen}/>
-      <main className="w-full flex flex-col items-center pt-8 gap-[45px] smlTab:pb-[103px] smlTab:gap-[55px] md:pb-[56px]">
+      <main className="w-full flex flex-col items-center pt-8 gap-[45px] smlTab:gap-[55px] smlTab:pb-[56px] smlTab:pt-12">
         <h1 className="text-2xl">pomodoro</h1>
         {/* CONTENT */}
-        <section className="w-full flex flex-col items-center px-6">
+        <div className="w-full flex flex-col items-center px-6">
           {/* STATE SELECTION BTNS */}
           <StateSelection />
           {/* MAIN TIMER SECTION */}
           <Timer />
           {/* SETTINGS ICON */}
           <SettingsBtn setIsSettingsModalOpen={setIsSettingsModalOpen} />
-        </section>
+        </div>
       </main>
     </div>
   );
